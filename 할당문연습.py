ad2f860fc9c38369187a2c8abb0b697e49b8a830
songi061 <msongi061@gmail.com>
2022-05-05 00:07:54 +0900

    예제 연습

---
+++ 할당문연습.py
@@ -0,0 +1,18 @@
+num1 = num2 = num3 = 200    #다중할당문
+print(num1,num2,num3) 
+
+num4 , num5 = 300, 400    #동시할당문
+print(num4,num5)
+
+num = 200
+num += 100    #200 + 100 연산을 수행한 결과의 값을 num에 저장  num=num+100과 같은 의미이다.
+print(num)
+
+num -= 100
+print(num)   # 300-100 연산을 수행한다. num = num-100 과 같다.
+
+num *= 20
+print(num)
+
+num /= 2
+print(num)
