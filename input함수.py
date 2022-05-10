95c194fdcdc8fe4145a0fb5165225b9d38806f7d
songi061 <104684923+songi061@users.noreply.github.com>
2022-05-10 15:16:30 +0900

    Add files via upload

---
+++ input함수.py
@@ -0,0 +1,4 @@
+name = input('이름을 입력하세요 :')   #name은 input함수를 통해 문자열로 입력받는다
+print('이름:',name)
+age = int(input('나이를 입력하세요 :'))  #age는 문자열로 입력받아 int형으로 변환한다
+print('10년 후 나이:', age+10)   #int형으로 변환됐기 때문에 덧셈 연산이 가능하다
