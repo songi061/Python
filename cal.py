d449c87ab5aecb114a2cd20392265dd5cae1eb68
songi061 <104684923+songi061@users.noreply.github.com>
2022-05-10 15:18:18 +0900

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,10 @@
+from tkinter import Y
+
+
+x = 100
+y = 200
+print(x + y) 
+print(x - y) 
+print(x / y) 
+print(x * y) 
+print(x ** 2) #**는 제곱을 의미한다.
---
+++ 변수선언.py
@@ -0,0 +1,6 @@
+name = "홍길동"
+print("이름:", name)
+width = 10
+height = 5
+rectangle_area = width * height 
+print("사각형의 면적 :", rectangle_area)
